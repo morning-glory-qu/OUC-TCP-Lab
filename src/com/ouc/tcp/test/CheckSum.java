2d1bde45e82c70879629524a83a6ef02629f26e1
hongboqu <2267606106@qq.com>
2025-12-27 21:04:47 +0800

    [RDT2.0] refactor(CheckSum.java)：重构Checksum

--- src/com/ouc/tcp/test/CheckSum.java
+++ src/com/ouc/tcp/test/CheckSum.java
@@ -1,27 +1,32 @@
 package com.ouc.tcp.test;
 
+import java.util.Set;
 import java.util.zip.CRC32;
 import com.ouc.tcp.message.TCP_HEADER;
 import com.ouc.tcp.message.TCP_PACKET;
+import com.ouc.tcp.message.TCP_SEGMENT;
 
 @SuppressWarnings("ALL")
 public class CheckSum {
+    /*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
+    public static short computeChkSum(TCP_PACKET tcpPack) {
+        CRC32 crc32 = new CRC32();
+        // 获取TCP首部信息
+        TCP_HEADER header = tcpPack.getTcpH();
+        int seq = header.getTh_seq();
+        int ack = header.getTh_ack();
+        // 将首部中序列号和确认号添加到CRC32计算中
+        crc32.update(seq);
+        crc32.update(ack);
 
-	/*计算TCP报文段校验和：只需校验TCP首部中的seq、ack和sum，以及TCP数据字段*/
-	public static short computeChkSum(TCP_PACKET tcpPack) {
-    CRC32 crc32 = new CRC32();
-    // 获取TCP首部信息
-    TCP_HEADER header = tcpPack.getTcpH();
-    // 将首部中序列号和确认号添加到CRC32计算中
-    crc32.update(header.getTh_seq());
-    crc32.update(header.getTh_ack());
-    // 数据部分
-    int[] data = tcpPack.getTcpS().getData();
-    for (int value : data) {
-        crc32.update(value);
+        // 数据部分
+        TCP_SEGMENT segment = tcpPack.getTcpS();
+        int[] data = segment.getData();
+        for (int value : data) {
+            crc32.update(value);
+        }
+        // 计算结果并返回
+        int checkSum = (int) crc32.getValue();
+        return (short) checkSum;
     }
-    // 计算结果并返回
-    return (short) crc32.getValue();
-    }
-
 }
