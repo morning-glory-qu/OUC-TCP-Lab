2b65db5a773495b499e63070899c734d1c19c1f9
hongboqu <2267606106@qq.com>
2025-12-28 01:33:30 +0800

    [RDT2.2] fix(TCP.Receiver.java): 修改了发重复ACK的一个小bug

--- src/com/ouc/tcp/test/TCP_Receiver.java
+++ src/com/ouc/tcp/test/TCP_Receiver.java
@@ -41,7 +41,7 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
             System.out.println("Receive Computed: " + CheckSum.computeChkSum(recvPack));
             System.out.println("Received Packet" + recvPack.getTcpH().getTh_sum());
             System.out.println("Problem: Packet Number: " + recvPack.getTcpH().getTh_seq() + " + InnerSeq:  " + sequence);
-            tcpH.setTh_ack(-1);
+            tcpH.setTh_ack(sequence);
             ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
             tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
             //回复ACK报文段
