2427987d4a4c00b02f093fafd1b8561b01a1566a
hongboqu <2267606106@qq.com>
2025-12-29 01:00:47 +0800

    [SR] feat(WindowElem)：完成WindowElem，供SenderElem和ReceiverElem继承。

--- src/com/ouc/tcp/test/WindowElem.java
+++ src/com/ouc/tcp/test/WindowElem.java
@@ -1,4 +1,36 @@
 package com.ouc.tcp.test;
 
+import com.ouc.tcp.message.TCP_PACKET;
+
 public class WindowElem {
+    protected  TCP_PACKET tcpPacket;
+    // 通用状态标记，可通过位运算表示多种状态
+    /*
+    0: 初始的空闲状态
+     */
+    protected  int flag;
+
+    public WindowElem() {
+        tcpPacket = null;
+        flag = 0;
+    }
+    public WindowElem(TCP_PACKET tcpPacket, int flag) {
+        this.tcpPacket = tcpPacket;
+        this.flag = flag;
+    }
+
+    public TCP_PACKET getTcpPacket() {
+        return tcpPacket;
+    }
+
+    public void setElem(TCP_PACKET tcpPacket, int flag) {
+        this.tcpPacket = tcpPacket;
+        this.flag = flag;
+    }
+
+    public void resetElem(){
+        tcpPacket = null;
+        flag = 0;
+    }
+
 }
