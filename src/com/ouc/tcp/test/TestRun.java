921665986e9934fa5de64967c4085a03d08fccdc
hongboqu <2267606106@qq.com>
2026-01-03 11:41:59 +0800

    [SR] feat(TestRun)：修改输出

--- src/com/ouc/tcp/test/TestRun.java
+++ src/com/ouc/tcp/test/TestRun.java
@@ -4,7 +4,7 @@ import com.ouc.tcp.app.SystemStart;
 
 public class TestRun {
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("启动RDT3.0：");
+		System.out.println("启动SR：");
 		SystemStart.main(null);
 		System.out.println("测试完成。");
 	}
