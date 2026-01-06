a67fcce64112da535195a4cf7630e873f86deaff
hongboqu <2267606106@qq.com>
2026-01-06 10:12:38 +0800

    [GBN] fix(SenderWindowElem)：不必为发送窗口放置全局计时器

--- src/com/ouc/tcp/test/SenderWindowElem.java
+++ src/com/ouc/tcp/test/SenderWindowElem.java
@@ -14,11 +14,9 @@ enum SenderFlag {
  * 管理数据包状态和重传机制
  */
 public class SenderWindowElem extends WindowElem {
-    private UDT_Timer timer; // 包重传计时器
 
     public SenderWindowElem() {
         super();
-        this.timer = null;
     }
 
     /** 返回包是否被确认 */
@@ -26,15 +24,8 @@ public class SenderWindowElem extends WindowElem {
         return this.flag == SenderFlag.ACKED.ordinal();
     }
 
-    /** 设置重传任务 */
-    public void scheduleTask(UDT_RetransTask task, int delay, int period) {
-        this.timer = new UDT_Timer();
-        this.timer.schedule(task, delay, period);
-    }
-
     /** 处理包确认：更新状态并停止计时器 */
     public void ackPacket() {
         this.flag = SenderFlag.ACKED.ordinal();
-        this.timer.cancel();
     }
 }
