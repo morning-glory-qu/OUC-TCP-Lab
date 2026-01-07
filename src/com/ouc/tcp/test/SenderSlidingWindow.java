95e3a86b2b1c591de0183b48b3b4ded1d23f6015
hongboqu <2267606106@qq.com>
2026-01-07 12:18:34 +0800

    [TCP-Tahoe] feat(SenderSlidingWindow)：差sendWindow没实现

--- src/com/ouc/tcp/test/SenderSlidingWindow.java
+++ src/com/ouc/tcp/test/SenderSlidingWindow.java
@@ -22,7 +22,11 @@ public class SenderSlidingWindow {
 
     public class GBN_RetransTask extends TimerTask {
         private final SenderSlidingWindow window;
-        public GBN_RetransTask(SenderSlidingWindow window) { this.window = window; }
+
+        public GBN_RetransTask(SenderSlidingWindow window) {
+            this.window = window;
+        }
+
         @Override
         public void run() {
             window.sendWindow();
@@ -30,18 +34,25 @@ public class SenderSlidingWindow {
     }
 
     public SenderSlidingWindow(UDT_Timer timer, TCP_Sender sender) {
-
+        this.sender = sender;
+        this.window = new LinkedBlockingDeque<>();
+        this.timer = new UDT_Timer();
     }
 
     private void sendWindow() {
+
     }
 
-    public void resetTimer(){
-
+    public void resetTimer() {
+        timer.cancel();
+        timer = new UDT_Timer();
+        if (!isEmpty()) {
+            timer.schedule(new GBN_RetransTask(this), delay, period);
+        }
     }
 
     public boolean isCwndFull() {
-
+        return window.size() >= cwnd;
     }
 
     public boolean isEmpty() {
@@ -49,18 +60,70 @@ public class SenderSlidingWindow {
     }
 
 
-    public void sendPacket(){
-
+    public void sendPacket() {
+        SenderWindowElem elem = window.poll();
+        if (elem == null) {
+            return;
+        }
+        if (!elem.isAcked()) {
+            sender.udt_send(elem.getTcpPacket());
+        }
+        window.push(elem);
     }
 
-    public void resendPacket(){
-
+    private void resendPacket(int ack) {
+        int expectedAck = ack + 100;
+        for (SenderWindowElem elem : window) {
+            if (elem.getTcpPacket().getTcpH().getTh_seq() == expectedAck) {
+                sender.udt_send(elem.getTcpPacket());
+                break;
+            }
+        }
     }
+
     public void pushPacket(TCP_PACKET packet) {
-
+        // 如果窗口为空，启动定时器
+        if (isEmpty()) {
+            timer = new UDT_Timer();
+            timer.schedule(new GBN_RetransTask(this), delay, period);
+        }
+        window.push(new SenderWindowElem(packet, SenderFlag.NOT_ACKED.ordinal()));
     }
 
-    public void ackPacket(TCP_PACKET packet) {
+
+    public void ackPacket(int ack) {
+        for (SenderWindowElem elem : window) {
+            if (elem.getTcpPacket().getTcpH().getTh_seq() <= ack) {
+                elem.ackPacket();
+                window.remove(elem);
+                if (cwnd < ssthresh) {
+                    cwnd++;
+                    dCwnd = cwnd;
+                }
+
+            }
+        }
+        resetTimer();
+
+        // 更新拥塞窗口
+        if (cwnd >= ssthresh) {
+            dCwnd += (double) 1 / cwnd;
+            cwnd = (int) dCwnd;
+        }
+
+        if (ack == lastAck) {
+            lastAckCount++;
+        } else {
+            lastAck = ack;
+            lastAckCount = 1;
+        }
+
+        if (lastAckCount >= lastAckCountLimit) {
+            ssthresh = cwnd / 2;
+            cwnd = 1;
+            dCwnd = (double) cwnd;
+            resendPacket(ack);
+        }
 
     }
 }
